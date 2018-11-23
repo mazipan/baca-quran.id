c8eab2b52d02989e8e80ee7214988ed94ef327d5
Irfan Maulana <irfan.maulana@bizzy.co.id>
2018-11-23 23:58:30 +0700

    fix title

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: `Quran Offline | ${pkg.description}`,
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
