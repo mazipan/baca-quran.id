cb43db0ab2c62aac3d7a2cc07097a5b9a1fe1cea
Irfan Maulana <mazipanneh@gmail.com>
2020-04-26 21:46:11 +0700

    Update index.vue

--- pages/_surahid/index.vue
+++ pages/_surahid/index.vue
@@ -21,9 +21,6 @@
         :prev-surah="prevSurah"
         :verse-count="Number(currentSurah.number_of_ayah)" />
     </div>
-
-    <script type="application/ld+json" v-html="jsonldBreadcrumb" />
-    <script type="application/ld+json" v-html="jsonLdArticle" />
   </section>
 </template>
 
@@ -99,7 +96,23 @@ export default class SurahDetailPage extends Vue {
           name: 'theme-color',
           content: this.settingActiveTheme.bgColor
         }
-      ]
+      ],
+      script: [
+        {
+          id: 'ld-breadcrumb',
+          innerHTML: JSON.stringify(this.jsonldBreadcrumb),
+          type: 'application/ld+json',
+          body: true
+        },
+        {
+          id: 'ld-article',
+          innerHTML: JSON.stringify(this.jsonLdArticle),
+          type: 'application/ld+json',
+          body: true
+        }
+
+      ],
+      __dangerouslyDisableSanitizers: ['script']
     }
   }
 
