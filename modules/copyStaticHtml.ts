bf871290b04a8300f9f8728e44f448cfd396f465
Irfan Maulana <mazipanneh@gmail.com>
2020-05-19 18:54:32 +0700

    Update copyStaticHtml.ts

--- modules/copyStaticHtml.ts
+++ modules/copyStaticHtml.ts
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
-import copyVerse from '../build-scripts/copyVerse'
+// import copyVerse from '../build-scripts/copyVerse'
 
 export default function copyStaticHtml () {
   // @ts-ignore
   this.nuxt.hook('generate:done', async () => {
     console.log('>>>>> Running modules copyStaticHtml')
-    await copyVerse()
+    // await copyVerse()
   })
 }
