698532b3739f650f1cc03c25ebbca33cbd3665fe
Irfan Maulana <maulana.irfan@tokopedia.com>
2021-01-02 22:07:18 +0700

    feat: remove sidebar head

--- components/BaseSidebar.vue
+++ components/BaseSidebar.vue
@@ -1,9 +1,6 @@
 <template>
   <aside class="sidebar">
     <div class="sidebar__content">
-      <div class="sidebar__head">
-        <img src="/icon.png" alt="Logo" height="50" width="50">
-      </div>
       <ul class="sidebar__menu">
         <li class="sidebar__item">
           <nuxt-link to="/?source=sidebar" class="sidebar__link" @click.native="navigateTo">
