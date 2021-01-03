a29867e3df4f820d558705ef5243fed6505903be
Irfan Maulana <mazipanneh@gmail.com>
2021-01-03 19:02:56 +0700

    Update getOfflineAssets.ts

--- build-scripts/getOfflineAssets.ts
+++ build-scripts/getOfflineAssets.ts
@@ -1,19 +1,5 @@
 const getOfflineAssets = (): string[] => {
-  const res: string[] = [
-    '/icon.png',
-    '/read-quran.svg',
-    '/icon_allah.svg',
-    '/icon_clock.svg',
-    '/icon_mosque.svg',
-    '/icon_prayer.svg',
-    '/icon_quran.svg',
-    '/icon_ramadhan.svg',
-    '/icon_rosary.svg',
-    '/illustration_1.jpg',
-    '/meta-image.png',
-    '/fonts/lpmq.otf',
-    '/fonts/lpmq.woff'
-  ]
+  const res: string[] = []
 
   return res
 }
