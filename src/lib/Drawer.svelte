2b9c40e4d8629e1b2a71416bf85ec4588a4d7cd7
Abizrh <abizarah857@gmail.com>
2023-08-12 00:00:46 +0700

    enhance: get active theme on different component

--- src/lib/Drawer.svelte
+++ src/lib/Drawer.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
 	import DrawerMenus from './DrawerMenus.svelte';
+	import { activeTheme } from '../store';
+	import { onDestroy } from 'svelte';
+	let currentTheme: string;
+
+	const unsubscribe = activeTheme.subscribe((value) => {
+		currentTheme = value;
+	});
+
+	onDestroy(() => {
+		unsubscribe();
+	});
 
 	export let open: boolean;
 	export let onToggle: () => void;
