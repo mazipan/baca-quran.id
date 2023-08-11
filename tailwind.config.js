546273b694c102f965e918786ae5da870e7ab73c
Abizrh <abizarah857@gmail.com>
2023-08-11 23:50:45 +0700

    chore: initiate tw-colors dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+const { createThemes } = require('tw-colors');
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-	darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        blueish: {
-          500: '#2E8A99',
-          600: '#142C66',
-          700: '#0E2954',
-          800: '#0a1d3d',
-          900: '#07162d',
-        },
-      }
-    },
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				blueish: {
+					500: '#2E8A99',
+					600: '#142C66',
+					700: '#0E2954',
+					800: '#0a1d3d',
+					900: '#07162d'
+				}
+			}
+		}
+	},
+	plugins: [
+		createThemes({
+			light: {
+				primary: 'steelblue'
+			},
+			dark: {
+				primary: 'turquoise'
+			},
+			yellow: {
+				primary: 'yellow'
+			},
+			sepia: {
+				primary: '#704214'
+			}
+		})
+	]
+};
